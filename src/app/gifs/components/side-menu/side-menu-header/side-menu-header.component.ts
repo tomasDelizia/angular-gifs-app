93f9eb95da52e4199d8c6f47eb18addcaa78558a
tdeliziae <tdeliziae@eservicios.indra.es>
2025-02-27 11:57:03 +0100

    Agregar entornos de desarrollo y producción

--- src/app/gifs/components/side-menu/side-menu-header/side-menu-header.component.ts
+++ src/app/gifs/components/side-menu/side-menu-header/side-menu-header.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'gifs-side-menu-header',
@@ -6,4 +7,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './side-menu-header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SideMenuHeaderComponent { }
+export class SideMenuHeaderComponent {
+  envs = environment;
+}
