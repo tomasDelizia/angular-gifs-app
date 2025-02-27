0abf09c86f48b6da113ab5e18c63ba7760b523c2
tdeliziae <tdeliziae@eservicios.indra.es>
2025-02-27 10:55:13 +0100

    Agregar rutas hijas del componente Dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,21 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     // Lazy loading de componentes
-    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component')
-  },
-  {
-    path: 'trending',
-    loadComponent: () => import('./gifs/pages/trending-page/trending-page.component')
-  },
-  {
-    path: 'search',
-    loadComponent: () => import('./gifs/pages/search-page/search-page.component')
+    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component'),
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () => import('./gifs/pages/trending-page/trending-page.component')
+      },
+      {
+        path: 'search',
+        loadComponent: () => import('./gifs/pages/search-page/search-page.component')
+      },
+      {
+        path: '**',
+        redirectTo: 'trending'
+      }
+    ]
   },
   {
     path: '**',
