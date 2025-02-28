8710694505faaec0f59513d57740330149201d21
tdeliziae <tdeliziae@eservicios.indra.es>
2025-02-28 10:26:27 +0100

    Guardar historial de búsquedas en el Local Storage

--- src/app/gifs/services/gif.service.ts
+++ src/app/gifs/services/gif.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { computed, inject, Injectable, signal } from '@angular/core';
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { GifMapper } from '../mapper/gif.mapper';
@@ -10,6 +10,7 @@ import { Gif } from '../interfaces/gif.interface';
   providedIn: 'root',
 })
 export class GifService {
+  private SEARCH_HISTORY_KEY = 'searchHistory';
   private http = inject(HttpClient);
 
   // Empleamos Records para almacenar un diccionario con el historial de búsqueda
@@ -19,7 +20,20 @@ export class GifService {
     Object.keys(this.searchHistory())
   );
 
+  saveSearchHistoryToLocalStorage = effect(() =>
+    localStorage.setItem(
+      this.SEARCH_HISTORY_KEY,
+      JSON.stringify(this.searchHistory())
+    )
+  );
+
+  private getSearchHistoryFromLocalStorage = (): Record<string, Gif[]> => {
+    const jsonSearchHistory = localStorage.getItem(this.SEARCH_HISTORY_KEY);
+    return jsonSearchHistory ? JSON.parse(jsonSearchHistory) : {};
+  };
+
   constructor() {
+    this.searchHistory.set(this.getSearchHistoryFromLocalStorage());
     console.log('GifService creado');
   }
 
@@ -56,6 +70,7 @@ export class GifService {
           // El operador tap es útil para el manejo de efectos secundarios. A diferencia de map, no modifica los datos
           // Historial de búsquedas
           tap((items) =>
+            // Operador spread para Records
             this.searchHistory.update((history) => ({
               ...history,
               [query.toLowerCase()]: items,
